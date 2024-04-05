c92c373d3cfde5d1f6b4c44fc6162bf2daaa7f9e
James Arnold <jamesnogra@gmail.com>
2024-04-05 21:18:25 +0800

    Change columns in user table and add voucher table.

---
+++ database/migrations/2024_04_05_131158_create_vouchers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('code', 5);
+            $table->timestamps();
+            // Add foreign key constraint
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Supposed to drop foreign key first but sqlite does not allow it
+        Schema::dropIfExists('vouchers');
+    }
+};
