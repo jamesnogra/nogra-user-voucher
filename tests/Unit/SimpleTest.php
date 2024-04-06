e4dacb2cfc387783a8c91a178aaff8e1bf5cb1d0
James Arnold <jamesnogra@gmail.com>
2024-04-06 14:07:59 +0800

    Add a simple unit test for the Github workflow.

---
+++ tests/Unit/SimpleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    /**
+     * A basic unit test example
+     * This was added to test the Github workflow
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
